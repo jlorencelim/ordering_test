ce0d1bbc90a0cbfab0563674e08882acb4afd03b
Lorence Lim <jlorencelim@gmail.com>
2018-06-14 14:20:10 +0800

    Implemented the logout view

--- users/views.py
+++ users/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.contrib.auth import logout
+from django.core.urlresolvers import reverse_lazy
+from django.views.generic import RedirectView
 
-# Create your views here.
+from braces.views import LoginRequiredMixin
+
+
+class LogoutView(LoginRequiredMixin, RedirectView):
+
+    def get_redirect_url(self):
+        logout(self.request)
+        return reverse_lazy('users:login')
+
