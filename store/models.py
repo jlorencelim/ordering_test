39e88b28759b34c4f74af886b7863d77b686cd80
Lorence Lim <jlorencelim@gmail.com>
2018-06-14 15:16:03 +0800

    Created the orders model

--- store/models.py
+++ store/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 
@@ -5,7 +6,15 @@ class Product(models.Model):
     symbol = models.CharField(max_length=60)
     description = models.TextField(blank=True)
     unit = models.CharField(max_length=10)
-    quantity = models.PositiveIntegerField(default=0)
 
     def __str__(self):
         return self.symbol
+
+
+class Order(models.Model):
+    user = models.ForeignKey(User, related_name='orders')
+    product = models.ForeignKey('store.Product', related_name='orders')
+    quantity = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return '{} {}'.format(self.user.get_full_name(), self.product.symbol)
--- store/urls.py
+++ store/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import url
 from store import views
 
 urlpatterns = [
-    url(r'^$', views.ProductView.as_view(), 'product_list'),
+    url(r'^$', views.ProductView.as_view(), name='product_list'),
 ]
